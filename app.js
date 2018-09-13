a83055b0caede44993c88450ebac778bf63574e4
dshu <hudongsheng@xfruit.cn>
2018-09-13 10:20:01 +0800

    add logger

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const path = require('path')
 const static = require('koa-static')
+const logger = require('koa-logger')
 const app = new Koa();
 
 const staticPath = 'public'
 
+app.use(logger())
 app.use(static(
     path.join(__dirname,staticPath)
 ))
