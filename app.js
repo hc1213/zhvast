760c4db902d59a5f79282d4acd2476c276a6471a
dshu <hudongsheng@xfruit.cn>
2018-09-13 09:49:24 +0800

    增加静态资源托管

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const Koa = require('koa');
+const path = require('path')
+const static = require('koa-static')
 const app = new Koa();
 
+const staticPath = '/public'
+
+app.use(static(
+    path.join(__dirname,staticPath)
+))
 const main = ctx => {
     ctx.response.body = 'hello zhvast';
 };
 
 app.use(main);
-app.listen(7001);
+app.listen(7001,()=>{
+    console.log('server is running at port 7001')
+});
---
+++ mock.js
@@ -0,0 +1,11 @@
+let Mock = require('mockjs')
+
+let data = Mock.mock({
+    'list|1-10':[
+        {
+            'id|+1':1
+        }
+    ]
+})
+
+console.log(JSON.stringify(data, null,4))
