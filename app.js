9ad1f4448466ff68f82f10552b43503a811e9ec6
dshu <hudongsheng@xfruit.cn>
2018-09-13 10:46:51 +0800

    debug

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ const Koa = require('koa');
 const path = require('path')
 const static = require('koa-static')
 const logger = require('koa-logger')
-const app = new Koa();
+const Router = require('koa-router')
+
+let app = new Koa();
+let router = new Router()
 
 const staticPath = 'public'
 
 app.use(logger())
+
+console.log('__dirname--->', path.join(__dirname, staticPath))
 app.use(static(
-    path.join(__dirname,staticPath)
+    path.join(__dirname, staticPath)
 ))
+
 // const main = ctx => {
 //     ctx.response.body = 'hello zhvast';
 // };
 
-// app.use(main);
+// router.get('/',async(ctx,next)=>{
+    
+// })
+
+
+// app.use(router.routes())
+// app.use(main)
 app.listen(7001,()=>{
     console.log('server is running at port 7001')
 });
