4e17d5396c704f7a44986af46ff65a1af3de4fb8
dshu <hudongsheng@xfruit.cn>
2018-09-13 10:00:36 +0800

    remove main

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const path = require('path')
 const static = require('koa-static')
 const app = new Koa();
 
-const staticPath = '/public'
+const staticPath = 'public'
 
 app.use(static(
     path.join(__dirname,staticPath)
 ))
-const main = ctx => {
-    ctx.response.body = 'hello zhvast';
-};
+// const main = ctx => {
+//     ctx.response.body = 'hello zhvast';
+// };
 
-app.use(main);
+// app.use(main);
 app.listen(7001,()=>{
     console.log('server is running at port 7001')
 });
